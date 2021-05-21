9e063005b17fc2d97346d2eedaf230633adaa4e4
👨🏾‍💻 Henry AT <yiperu@gmail.com>
2021-05-21 13:52:36 -0400

    Make View and linked with binding

--- app/src/main/java/com/example/architecturecomponents/MainActivity.kt
+++ app/src/main/java/com/example/architecturecomponents/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.architecturecomponents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.architecturecomponents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.liveDataButton.setOnClickListener {
+
+        }
+
+
     }
 }
